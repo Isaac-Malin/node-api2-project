7bd5f7d64312ad8f2983c8b80a4ba2141c2561f2
Isaac Malin <Isaacmalin3530@gmail.com>
2024-02-24 02:29:13 -0700

    getting all the posts from the first path is now working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,18 @@
 // implement your posts router here
+const express = require('express')
+const posts = require('./posts-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  posts.find()
+    .then(posts => {
+      res.status(200).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
+})
+
+
+module.exports = router
